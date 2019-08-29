23f3680a261d46f0471ae0d514471d71b82215e6
Greg Herbowicz <herbowicz@gmail.com>
2019-08-29 15:06:07 +0200

    nice comments :D

--- my-jwt.js
+++ my-jwt.js
@@ -3,7 +3,11 @@ const jwt = require("jsonwebtoken");
 var data = {
   id: 10
 };
-var token = jwt.sign(data, "123abc");
-var decoded = jwt.verify(token, "123abc");
+
+//jwt.sign() takes the object and signs it, it creates the hash and returns the token value.
+var token = jwt.sign(data, "my-secret-123");
+
+//jwt.verify() does the opposite, it takes that token and the secret to ensure data wasn't manipulated.
+var decoded = jwt.verify(token, "my-secret-123");
 
 console.log("jwt token", token, " decoded:", decoded);
