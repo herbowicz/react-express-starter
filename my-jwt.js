246724dfb13e59d30b06821955afc28e80f33237
Greg Herbowicz <herbowicz@gmail.com>
2019-08-29 15:14:10 +0200

    descr.

--- my-jwt.js
+++ my-jwt.js
@@ -8,11 +8,14 @@ var data = {
 var token = jwt.sign(data, "my-secret-123");
 
 //jwt.verify() does the opposite, it takes that token and the secret to ensure data wasn't manipulated.
-
 try {
-  var decoded = jwt.verify(token, "my-secret-1223"); //WRONG SECRET!!
+  var decoded = jwt.verify(token, "my-secret-123");
 } catch (e) {
+  //when invalid secret
   console.log("error", e);
 }
 
 console.log("jwt token", token, " decoded:", decoded);
+
+//1. We need to send user authentication token post signup and login calls.
+//2. Later client will send this token with the call to get data, and we will authenticate this token to see if user has the access.
